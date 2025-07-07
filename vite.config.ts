8c7b2e37279ec1f72cd48cea8b7744e9024b4213
Yass <ynezreg@gmail.com>
2025-07-07 18:29:53 +0200

    fix: reconfigure base for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath, URL } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
-  base: "/Armando_Project/", // <-- ajoute cette ligne
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: { "@": path.resolve(__dirname, "./src") },
   },
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `
-          @use "src/styles/variables" as *;
-          @use "src/styles/mixins" as *;
+          @use "@/styles/variables" as *;
+          @use "@/styles/mixins"    as *;
         `,
       },
     },
