896f02e94696892944118551a1223399bd3c5c9a
Yass <ynezreg@gmail.com>
2025-07-07 18:42:37 +0200

    fix: vite config dynamic base for local dev and GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { fileURLToPath, URL } from "url";
 
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
-
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   resolve: {
-    alias: { "@": path.resolve(__dirname, "./src") },
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `
           @use "@/styles/variables" as *;
-          @use "@/styles/mixins"    as *;
+          @use "@/styles/mixins" as *;
         `,
       },
     },
   },
-});
+  base: mode === "production" ? "/Armando_Project/" : "/",
+}));
